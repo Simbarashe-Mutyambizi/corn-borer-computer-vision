e518fe4c3b90da7a4eba242121bbb5f21126d2ea
Simbarashe-Mutyambizi <123954442+Simbarashe-Mutyambizi@users.noreply.github.com>
2025-01-06 15:55:17 +0200

    Update model_downloader.py

--- model_downloader.py
+++ model_downloader.py
@@ -6,7 +6,7 @@ Created on Sat Jan  4 12:13:58 2025
 """
 import os
 import dagshub
-import pickle
+import joblib
 import mlflow
 
 model_path=os.path.join(os.getcwd(),"model")
@@ -20,4 +20,4 @@ dagshub.auth.add_app_token(token="c751b2e61cc4e223562618c9fa391e9b3c6ab576")
 dagshub.init(repo,name)
 ml=mlflow.pyfunc.load_model("mlflow-artifacts:/832dce8f9c7d4ada8db55eb01924cc2e/18c7324dd47f4336b53e63703690e4ef/artifacts/model")
 with open('saved_model.pkl', 'wb') as f:
-    pickle.dump(ml, f)
+    joblib.dump(ml, f)
