4d3d15aa4ea1de88f6a19b0d10e547d4c239b915
Simbarashe-Mutyambizi <123954442+Simbarashe-Mutyambizi@users.noreply.github.com>
2025-01-06 15:42:20 +0200

    Update model_downloader.py

--- model_downloader.py
+++ model_downloader.py
@@ -5,8 +5,19 @@ Created on Sat Jan  4 12:13:58 2025
 @author: SWRM
 """
 import os
-import wget
+import dagshub
+import pickle
+import mlflow
+
 model_path=os.path.join(os.getcwd(),"model")
 os.makedirs(model_path,exist_ok=True)
 os.chdir(model_path)
-wget.download("mlflow-artifacts:/832dce8f9c7d4ada8db55eb01924cc2e/18c7324dd47f4336b53e63703690e4ef/artifacts/model/data/model.keras")
+
+
+repo="my-first-repo"
+name="Simbarashe-Mutyambizi"
+dagshub.auth.add_app_token(token="c751b2e61cc4e223562618c9fa391e9b3c6ab576")
+dagshub.init(repo,name)
+ml=mlflow.pyfunc.load_model("mlflow-artifacts:/832dce8f9c7d4ada8db55eb01924cc2e/18c7324dd47f4336b53e63703690e4ef/artifacts/model")
+with open('saved_model.pkl', 'wb') as f:
+    pickle.dump(ml, f)
